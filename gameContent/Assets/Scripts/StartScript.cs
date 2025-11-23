a8ead8283e0cd0e98229f1ad07aa17cb602a3730
Zimon Roos <zimon.roos@icloud.com>
2025-11-23 14:03:05 +0100

    Add StartScript class with basic structure and logging

---
+++ gameContent/Assets/Scripts/StartScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("StartScript is running!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lägg din logik här
+    }
+}
